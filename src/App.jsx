b16ea7ed2972850bff46945270aa9f1599305f9b
Ad-cmd-1976 <adityavishwakarma9025@gmail.com>
2025-08-30 14:46:57 +0530

    Card Flipping logic written

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useHelperStore } from './store/useHelperStore.js';
 
 function App() {
-  const { cards, generateCards }=useHelperStore();
+  const { cards, generateCards, handleFlip, moves, best }=useHelperStore();
   useEffect(()=>{
     generateCards();
   },[])
@@ -10,15 +10,19 @@ function App() {
     <div className="flex flex-col items-center min-h-screen bg-gray-900 text-white">
       
       <div className="flex justify-between w-full max-w-3xl p-4 text-lg">
-        <div>Moves: 0</div>
+        <div>{`Moves: ${moves}`}</div>
         <div>Time: 00:00</div>
-        <div>Best: 15 moves</div>
+        <div>{`Best: ${best}`}</div>
       </div>
 
       <div className="grid grid-cols-4 gap-4 p-4 w-full max-w-3xl border border-gray-700 rounded-lg bg-gray-800">
         { cards.map((card, index)=>(
-          <div key={cards.id} className={`w-20 h-28 flex items-center justify-center rounded-lg cursor-pointer`}>
-            { ((card.isflipped || card.isMatched)? card.value : "?")}
+          <div key={card.id} 
+          className={`w-20 h-28 flex items-center justify-center rounded-lg cursor-pointer
+          ${(card.isFlipped || card.isMatched) ? "bg-pink-500" : "bg-blue-600"}`}
+          onClick={()=>handleFlip(index)}
+          >
+            { ((card.isFlipped || card.isMatched)? card.value : "?")}
           </div>
         )) }
       </div>
--- src/store/useHelperStore.js
+++ src/store/useHelperStore.js
@@ -2,18 +2,60 @@ import { create } from "zustand";
 
 export const useHelperStore=create((set,get)=>({
     cards:[],
+    flipped:[],
     moves:0,
     timer:0,
     best:0,
+    game: "idle",
 
     generateCards:()=>{
         const arr=[1,2,3,4,5,6,7,8];
-        const deck=arr.sort(() => Math.random-0.8).map((val,ind)=>({
+        const deck=[...arr,...arr].sort(() => Math.random-0.8).map((value,ind)=>({
             id:ind,
-            val,
+            value,
             isFlipped:false,
             isMatched:false
         }))
         set({ cards:deck });
+    },
+
+    handleFlip:(index)=>{
+        const card=get().cards[index];
+        if(get().flipped.length===2 || card.isFlipped || card.isMatched) return;
+        
+        const newCards=get().cards;
+        newCards[index].isFlipped=true;
+        set({ cards: newCards });
+        
+        const newFlipped=[...get().flipped, index];
+        set({ flipped: newFlipped });
+        if(newFlipped.length==2){
+            set({ moves: get().moves+1 });
+            const [ind1, ind2]=newFlipped;
+            if(newCards[ind1].value===newCards[ind2].value){
+                newCards[ind1].isMatched=true;
+                newCards[ind2].isMatched=true;
+                set({ cards: newCards, flipped: [] });
+                get().isWon(newCards);
+            }
+            else{
+                setTimeout(()=>{
+                    newCards[ind1].isFlipped=false;
+                    newCards[ind2].isFlipped=false;
+                    set({ cards: newCards, flipped: [] });
+                }, 1000)
+            }
+        }
+    },
+
+    isWon:(cards)=>{
+        if(cards.every(card=>card.isMatched)){
+            set({ game: "won" });
+            const moves=get().moves;
+            if(moves+1<get().best){
+                set({ best: moves+1 });
+                localStorage.setItem("Best", get().moves+1);
+            }
+        }
     }
 }))
